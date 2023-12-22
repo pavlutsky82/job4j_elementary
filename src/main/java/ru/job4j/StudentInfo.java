e7e3a8af492a851ce490965a68b933261cb3ef17
pavlutsky <pavlutsky@mail.ru>
2023-12-22 22:35:27 +0800

    Загрузка файлов на github

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Roman Pavlutsky");
+        System.out.println("16.04.1982");
+    }
+}
